b2ee6f097cb0cd7ec43160d291e60051017a1558
Sina Haghighi <sina.liana.2001@gmail.com>
2022-02-18 22:31:59 -0800

    updated config file

--- application/__init__.py
+++ application/__init__.py
@@ -1,10 +1,10 @@
 from operator import index
 from flask import Flask, render_template
-# from config import Config
+from config import Config
 
 def create_app():
     app = Flask(__name__)
-    # app.config.from_object(Config)
+    app.config.from_object(Config)
 
     from application.main.main import main as main_blueprint
 
