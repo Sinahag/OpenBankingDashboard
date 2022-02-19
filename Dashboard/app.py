9d232702f044395c5cfde5915d8041bb625f4aeb
Sina Haghighi <sina.liana.2001@gmail.com>
2022-02-18 21:38:34 -0800

    update files with home page and route __init__

---
+++ Dashboard/app.py
@@ -0,0 +1,3 @@
+from application.__init__ import create_app
+
+app = create_app()
---
+++ Dashboard/application/main/main.py
@@ -0,0 +1,9 @@
+import os
+from flask import render_template, Blueprint
+
+main = Blueprint('main',__name__)
+
+@main.route("/")
+@main.route("/home")
+def index():
+    return render_template("index.html",index=True)
