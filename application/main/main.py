c3286d357eeef248163ae7c92f12fcdbd98b53b9
Sina <sina.liana.2001@gmail.com>
2022-02-19 18:49:46 -0800

    Added dashboard page and authentication files

--- application/main/main.py
+++ application/main/main.py
@@ -1,9 +1,20 @@
 import os
 from flask import render_template, Blueprint
+# from flask_login import login_required
+main = Blueprint('main', __name__)
 
-main = Blueprint('main',__name__)
 
 @main.route("/")
 @main.route("/home")
+@main.route("/index")
+# @login_required
 def index():
-    return render_template("index.html",index=True, title="Home")
+    return render_template("index.html", index=True)
+
+
+@main.route("/dashboard")
+# @login_required
+def dashboard():
+    return render_template("dashboard.html", username='User')
+
+# create the follow up pages for the dashboard (where the dashboard leadas to)
